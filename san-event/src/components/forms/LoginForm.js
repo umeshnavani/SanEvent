b7009154a7d160f2435c801303c550661f4a658f
unavani <umesh.navani@gmail.com>
2019-12-31 18:42:03 -0500

    #1: Login form

--- san-event/src/components/forms/LoginForm.js
+++ san-event/src/components/forms/LoginForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Form, Button } from "semantic-ui-react";
+import PropTypes from 'prop-types';
+import { Form, Button, Message } from "semantic-ui-react";
 import validator from 'validator';
 import InlineError from "../messages/InlineError";
-import PropTypes from 'prop-types';
 
 class LoginForm extends React.Component {
     state = {
@@ -23,9 +23,16 @@ class LoginForm extends React.Component {
         this.setState({ errors });
 
         if(Object.keys(errors).length === 0) {
-            this.props.submit(this.state.data);
+            this.setState({ loading: true });
+            this.props
+                .submit(this.state.data)
+                .catch(err => this.setState({ 
+                    errors: err.response.data.errors,
+                    loading: false
+                }
+            ));
         }
-    }
+    };
 
     validate = (data) => {
         const errors = {};
@@ -37,9 +44,15 @@ class LoginForm extends React.Component {
     }
 
     render() {
-        const { data, errors } = this.state;
+        const { data, errors, loading } = this.state;
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+             {errors.global && (
+                 <Message negative>
+                    <Message.Header>Something went wrong</Message.Header>
+                    <p>{errors.global}</p>
+                 </Message>
+             )}
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input 
