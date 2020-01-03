4e837ee541b8424ad816fb8d53449c4ce450a13c
unavani <umesh.navani@gmail.com>
2020-01-02 20:09:37 -0500

    #1: Setup routes

---
+++ san-event/src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Message } from "semantic-ui-react";
+
+const ConfirmEmailMessage = () => (
+  <Message info>
+    <Message.Header>
+      Please verify your email address
+    </Message.Header>
+  </Message>
+);
+
+export default ConfirmEmailMessage;
