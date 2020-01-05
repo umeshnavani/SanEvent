36ba3c3caa4175314e4b5c0f40da635c7b63dd13
unavani <umesh.navani@gmail.com>
2020-01-05 00:04:58 -0500

    #6: Reset password

--- san-event/src/actions/auth.js
+++ san-event/src/actions/auth.js
@@ -23,6 +23,14 @@ export const logout = () => (dispatch) => {
         
 export const confirm = token => dispatch =>
     api.user.confirm(token).then(user => {
-    localStorage.bookwormJWT = user.token;
+    localStorage.saneventJWT = user.token;
     dispatch(userLoggedIn(user));
 });
+
+
+export const resetPasswordRequest = ({ email }) => () =>
+  api.user.resetPasswordRequest(email);
+
+export const validateToken = token => () => api.user.validateToken(token);
+
+export const resetPassword = data => () => api.user.resetPassword(data);
